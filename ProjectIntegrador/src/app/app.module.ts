245b8735822b524380ff919fbe60a9d22cf032a2
Bruno2204 <brunoegama@gmail.com>
2022-10-09 14:41:54 -0300

    acerca de por la mitad

--- ProjectIntegrador/src/app/app.module.ts
+++ ProjectIntegrador/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AplogoComponent } from './components/aplogo/aplogo.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     AplogoComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
