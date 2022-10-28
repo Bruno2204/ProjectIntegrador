ea99d4836224d722e5ccbc7734c7816fc3ae8e76
Bruno2204 <brunoegama@gmail.com>
2022-10-28 14:27:20 -0300

    Backend

---
+++ ProjectIntegrador/BackEnd/src/main/java/com/portfolio/bruno/Interface/IPersonaService.java
@@ -0,0 +1,15 @@
+package com.portfolio.bruno.Interface;
+
+import com.portfolio.bruno.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    
+    public List<Persona> getPersona();
+    
+    public void savePersona(Persona persona);
+    
+    public void deletePersona(Long id);
+    
+    public Persona findPersona(Long id);
+}
---
+++ ProjectIntegrador/BackEnd/src/test/java/com/portfolio/bruno/BrunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.bruno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
